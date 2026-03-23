4a62cbec856260170037509a457db9922cf59b5a
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2026-03-23 15:21:25 +0200

    New: Enhance through adding size, height and order methods

--- Practical_17/tryBST.java
+++ Practical_17/tryBST.java
@@ -64,4 +64,62 @@ public class tryBST {
 
         return node;
     }
+
+    public boolean isBST() {
+        return is_BST_rec(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean is_BST_rec(tNode node, int min, int max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.key <= min || node.key >= max) {
+            return false;
+        }
+
+        return is_BST_rec(node.left, min, node.key) && is_BST_rec(node.right, node.key, max);
+    }
+
+    public int size() {
+        return size_rec(root);
+    }
+
+    private int size_rec(tNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        return 1 + size_rec(node.left) + size_rec(node.right);
+    }
+
+    public int height() {
+        return height_rec(root);
+    }
+
+    private int height_rec(tNode node) {
+        if (node == null) {
+            return -1;
+        }
+
+        return 1 + Math.max(height_rec(node.left), height_rec(node.right));
+    }
+
+    public void in_order() {
+        in_order_rec(root);
+
+        System.out.println();
+    }
+
+    private void in_order_rec(tNode node) {
+        if (node == null) {
+            return;
+        }
+
+        in_order_rec(node.left);
+
+        System.out.print(node.key + " ");
+
+        in_order_rec(node.right);
+    }
 }
