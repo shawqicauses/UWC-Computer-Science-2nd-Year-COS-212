c86460c46c69066bb26439a69da7826ff0fad84d
Shawqi Hatem - Developer and Instructor <shawqicauses@outlook.com>
2026-03-23 14:53:56 +0200

    New: Enhance through adding t node class

---
+++ Practical_17/tNode.java
@@ -0,0 +1,12 @@
+package Practical_17;
+
+public class tNode {
+    int key;
+    tNode left, right;
+
+    public tNode(int key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
